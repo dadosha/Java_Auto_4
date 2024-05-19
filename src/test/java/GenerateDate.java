a26587e4a928eb42d6028d72efff6b339088413e
Станислав Койфман <stanislavkoyfman@gmail.com>
2024-05-19 13:38:53 +0300

    Add check data

--- src/test/java/GenerateDate.java
+++ src/test/java/GenerateDate.java
@@ -1,9 +1,9 @@
-package ru.netology.autojava4;
-
 import java.time.Instant;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class GenerateDate {
     public String dateCreate(long days) {
@@ -11,9 +11,9 @@ public class GenerateDate {
         return timeNow.format(DateTimeFormatter.ofPattern("dd.MM.yyyy"));
     }
 
-    public String dateTimestampSevenDays() {
+    public List<String> dateTimestampSevenDays() {
         LocalDate timeNow = LocalDate.now().plusDays(7);
         Instant instant = timeNow.atStartOfDay(ZoneId.systemDefault()).toInstant();
-        return String.valueOf(instant.getEpochSecond() * 1000);
+        return List.of(String.valueOf(instant.getEpochSecond() * 1000), timeNow.format(DateTimeFormatter.ofPattern("dd.MM.yyyy")));
     }
 }
--- src/test/java/TestApp.java
+++ src/test/java/TestApp.java
@@ -1,8 +1,8 @@
 import org.junit.jupiter.api.Test;
 import org.openqa.selenium.Keys;
-import ru.netology.autojava4.GenerateDate;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Random;
 
 import static com.codeborne.selenide.Condition.*;
@@ -12,6 +12,7 @@ import static com.codeborne.selenide.Selenide.*;
 public class TestApp {
     GenerateDate dateCreate = new GenerateDate();
     Random rnd = new Random();
+    String date = dateCreate.dateCreate(rnd.nextInt(5) + 3);
 
     @Test
     void positiveTests() {
@@ -19,13 +20,14 @@ public class TestApp {
         $("[data-test-id='city'] input").setValue("Майкоп");
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(dateCreate.dateCreate(rnd.nextInt(5) + 3));
+        $("[data-test-id='date'] input").setValue(date);
         $("[data-test-id='name'] input").setValue("Койфман Станислав-Богданович");
         $("[data-test-id='phone'] input").setValue("+79999999999");
         $("[data-test-id='agreement']").click();
         $("button.button_view_extra").click();
         $("button.button_view_extra .spin_visible").shouldBe(visible);
         $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 
     @Test
@@ -35,29 +37,33 @@ public class TestApp {
         $$(".popup .menu-item span").find(exactText("Майкоп")).click();
         $("[data-test-id='date'] input").sendKeys(Keys.CONTROL + "a");
         $("[data-test-id='date'] input").sendKeys(Keys.BACK_SPACE);
-        $("[data-test-id='date'] input").setValue(dateCreate.dateCreate(rnd.nextInt(5) + 3));
+        $("[data-test-id='date'] input").setValue(date);
         $("[data-test-id='name'] input").setValue("Койфман Станислав-Богданович");
         $("[data-test-id='phone'] input").setValue("+79999999999");
         $("[data-test-id='agreement']").click();
         $("button.button_view_extra").click();
         $("button.button_view_extra .spin_visible").shouldBe(visible);
         $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + date));
     }
 
     @Test
     void positiveDateTests() {
+        List<String> dateList = dateCreate.dateTimestampSevenDays();
+
         open("http://localhost:9999/");
         $("[data-test-id='city'] input").setValue("Майкоп");
         $("[data-test-id='date'] input").click();
-        if (!($(String.format("[data-day='%s']", dateCreate.dateTimestampSevenDays())).isDisplayed())) {
+        if (!($(String.format("[data-day='%s']", dateList.get(0))).isDisplayed())) {
             $(".calendar__arrow_direction_right[data-step='1']").click();
         }
-        $(String.format("[data-day='%s']", dateCreate.dateTimestampSevenDays())).click();
+        $(String.format("[data-day='%s']", dateList.get(0))).click();
         $("[data-test-id='name'] input").setValue("Койфман Станислав-Богданович");
         $("[data-test-id='phone'] input").setValue("+79999999999");
         $("[data-test-id='agreement']").click();
         $("button.button_view_extra").click();
         $("button.button_view_extra .spin_visible").shouldBe(visible);
         $(withText("Встреча успешно забронирована")).shouldBe(visible, Duration.ofSeconds(15));
+        $("[data-test-id='notification'] .notification__content").shouldHave(exactText("Встреча успешно забронирована на " + dateList.get(1)));
     }
 }
